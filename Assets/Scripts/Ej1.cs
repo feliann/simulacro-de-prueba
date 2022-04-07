9c2686c1703e203a5d90077ac1c74736abf9eda6
feliann <101577329+feliann@users.noreply.github.com>
2022-04-07 08:32:04 -0300

    Finished all

---
+++ Assets/Scripts/Ej1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//1.Crea un programa que pida dos números al usuario y responda si son iguales o no lo son.
+
+public class Ej1 : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public int num1;
+    public int num2;
+    void Start()
+    {
+        if (num1== num2)
+        {
+            Debug.Log("Los dos numeros son iguales");
+        }
+        else
+        {
+            Debug.Log("los numeros son diferentes");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+
+    //2.Solicitar el ingreso del precio de 3 productos y un monto de dinero disponible
+    //.Comunicar si la suma de los mismos supera o no el monto de dinero disponible.
+    //Luego indicar cuánto dinero sobra o falta.
+{
+    // Start is called before the first frame update
+    public float precio1;
+    public float precio2;
+    public float precio3;
+    public float monto;
+    void Start()
+    {
+            if (precio1+precio2+precio3<monto)
+        {
+            Debug.Log("Podes comprar los tres productos con la plata que tenes");
+        }
+            else
+        {
+            Debug.Log("No tenes suficientes plata para comprar los tres productos");
+        }
+        if (precio1 + precio2 + precio3 < monto)
+        {
+            float res1;
+            res1 = monto - (precio1 + precio2 + precio3);
+            Debug.Log("te sobra" + res1);
+        }
+        else if (precio1 + precio2 + precio3 > monto)
+        {
+            float res2;
+            res2 = (precio1 + precio2 + precio3)-monto;
+            Debug.Log("te sobra" + res2);
+        }
+    }
+
+   
+}
